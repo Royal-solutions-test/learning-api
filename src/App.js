2ce1a17be51cebd8caa7970363fb262ebab622b1
Kingsley Goodluck <goodluckcanhelp@gmail.com>
2023-01-21 16:19:00 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useEffect } from "react";
 
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 function App() {
+  const [property, setProperty] = useState([]);
+  const fetchData = async () => {
+    try {
+      const apiCall = await axios.get(
+        "http://192.168.43.156:6414/api/v1/property/29"
+      );
+      console.log(apiCall);
+
+      if (apiCall.data.data) {
+        setProperty(apiCall.data.data);
+      } else {
+        console.log("NO data");
+      }
+    } catch (err) {
+      console.log(err.response);
+      // return err.response;
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {property.map((p, index) => {
+          // if (p.)
+          return (
+            <>
+              <h1> {p.j_title}</h1>
+
+              <h2>
+                {p.Plots && p.Plots.length > 0 ? (
+                  p.Plots.map((plot) => <p> {plot.j_city}</p>)
+                ) : (
+                  <p> there is no plot for this estate</p>
+                )}
+              </h2>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
